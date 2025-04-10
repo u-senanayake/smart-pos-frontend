2dbda2b1dda737db776a39734659ebacb9a14d19
Udayanga Senanayake <mailtoudayanga@gmail.com>
2025-04-10 15:00:08 +0900

    Added Tooltip for table actions

--- src/components/PageElements/IconButtons.jsx
+++ src/components/PageElements/IconButtons.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { IconButton } from "@mui/material";
-import { Delete, Edit, Preview } from "@mui/icons-material";
+import { IconButton, Tooltip, } from "@mui/material";
+import { Delete, Edit, Preview, } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
 export const DeleteIcon = ({ onClick, }) => {
     return (
-        <IconButton onClick={onClick} aria-label="Delete">
-            <Delete color="error" />
-        </IconButton>
+        <Tooltip title="Delete">
+            <IconButton onClick={onClick} aria-label="Delete">
+                <Delete color="error" />
+            </IconButton>
+        </Tooltip>
     );
 };
 
 export const EditIcon = ({ url, }) => {
     return (
-        <IconButton component={Link} to={url} aria-label="Edit">
-            <Edit color="primary" />
-        </IconButton>
+        <Tooltip title="Edit">
+            <IconButton component={Link} to={url} aria-label="Edit">
+                <Edit color="primary" />
+            </IconButton>
+        </Tooltip>
     );
 };
 
 export const PreviewIcon = ({ url, ariaLabel = "Preview Role" }) => {
     return (
-        <IconButton component={Link} to={url} aria-label={ariaLabel}>
-            <Preview color="primary" />
-        </IconButton>
+        <Tooltip title="View">
+            <IconButton component={Link} to={url} aria-label={ariaLabel}>
+                <Preview color="primary" />
+            </IconButton>
+        </Tooltip>
     );
 };
--- src/utils/const/FieldProperty.jsx
+++ src/utils/const/FieldProperty.jsx
@@ -1,5 +1,5 @@
 //Role
-export const ROLE_NAME_MIN = 10;
+export const ROLE_NAME_MIN = 5;
 export const ROLE_NAME_MAX = 25;
 export const ROLE_DESCRIPTION_MIN = 10;
 export const ROLE_DESCRIPTION_MAX = 100;
